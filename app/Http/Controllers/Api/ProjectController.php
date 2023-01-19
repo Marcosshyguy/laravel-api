f987c3e031a1b9792a6afbced13f31c0e86a764c
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-01-19 16:30:15 +0100

    create: controller to manage api calls

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json([
+            'succes' => true,
+            'results' => $projects,
+        ]);
+    }
+}
